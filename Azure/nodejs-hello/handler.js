64438db63443e5ffc79fecc17af7d5d04be846f8
Liam Barnett <liam.barnett@uk.fujitsu.com>
2018-03-15 09:02:03 +0000

    nodejs azure function, created using serverless framework

---
+++ Azure/nodejs-hello/handler.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/* eslint-disable no-param-reassign */
+
+module.exports.hello = function (context) {
+  context.log('JavaScript HTTP trigger function processed a request.');
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: 'Go Serverless v1.x! Your function executed successfully1!',
+    headers: {'Content-Type': 'application/json' }
+  };
+
+  context.done();
+};
